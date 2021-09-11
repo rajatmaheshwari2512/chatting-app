c3f514248cea60df61f7bf9575860ea4714ebde2
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-09-11 22:48:23 +0530

    Completed joining rooms with roomid

--- frontend/src/Components/Sidebar/Sidebar.js
+++ frontend/src/Components/Sidebar/Sidebar.js
@@ -13,6 +13,8 @@ const Sidebar = ({
   currentRoom,
 }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isJoinVisible, setIsJoinVisible] = useState(false);
+  const [joinRoomId, setJoinRoomId] = useState("");
   const [roomName, setRoomName] = useState("");
   const history = useHistory();
   useEffect(() => {
@@ -21,7 +23,6 @@ const Sidebar = ({
         console.log(currentRoom, roomid);
         if (currentRoom !== roomid) {
           var temp = rooms.slice(0);
-          console.log("Inside Socket", currentRoom);
           for (var i = 0; i < temp.length; i++) {
             if (temp[i].roomid === roomid && temp[i].roomid !== currentRoom) {
               temp[i].colour = "#013220";
@@ -42,7 +43,6 @@ const Sidebar = ({
   }, [socket, currentRoom]);
   const handleClick = (e) => {
     var temp = rooms.slice(0);
-    console.log("Inside Click", e.target.className.split(" ")[1]);
     for (var i = 0; i < temp.length; i++) {
       if (
         temp[i].roomid === e.target.className.split(" ")[1] &&
@@ -87,11 +87,48 @@ const Sidebar = ({
       });
     }
   };
+  const handleJoin = () => {
+    if (localStorage.getItem("token")) {
+      const data = {
+        roomid: joinRoomId,
+        name: null,
+      };
+      const config = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      };
+      axios
+        .post(process.env.REACT_APP_API_URL + "/users/room", data, config)
+        .then((resp) => {
+          var temp = rooms.slice(0);
+          temp.push(resp.data.room);
+          setRooms(temp);
+          socket.emit("room", { roomid: resp.data.room.roomid });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      setIsJoinVisible(false);
+    } else {
+      history.push("/");
+      notification.error({
+        message: "Please Login First",
+        description: "You are not logged in",
+      });
+    }
+  };
   const handleOpen = () => {
     setIsModalVisible(true);
   };
   const handleCancel = () => {
     setIsModalVisible(false);
+  };
+  const handleJoinCancel = () => {
+    setIsJoinVisible(false);
+  };
+  const handleJoinOpen = () => {
+    setIsJoinVisible(true);
   };
   return (
     <Row style={{ marginTop: "20%" }}>
@@ -120,6 +157,33 @@ const Sidebar = ({
           placeholder="Name your Chatroom"
           value={roomName}
           onChange={(e) => setRoomName(e.target.value)}
+        />
+      </Modal>
+      <div
+        onClick={handleJoinOpen}
+        className="join-room"
+        style={{
+          height: "60px",
+          width: "100%",
+          borderTop: "1px solid gray",
+          textAlign: "center",
+          cursor: "pointer",
+        }}
+      >
+        <h3>
+          <b>Join a Chat Room</b>
+        </h3>
+      </div>
+      <Modal
+        title="Enter the Room ID"
+        visible={isJoinVisible}
+        onOk={handleJoin}
+        onCancel={handleJoinCancel}
+      >
+        <Input
+          placeholder="Enter the Chatroom ID"
+          value={joinRoomId}
+          onChange={(e) => setJoinRoomId(e.target.value)}
         />
       </Modal>
       {rooms &&
